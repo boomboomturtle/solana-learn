2eece6c91b3322bf27cf4881fb69be4a69458b39
boomboom <boomboomturtle@gmail.com>
2025-06-22 11:59:51 +0800

    Created key pair for devnet

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+const DEVNET_URL: &str = "https://api.devnet.solana.com";
+const PUB_KEY: &str = "2HbxvgWoxeRJHKcxjReCWtMPg6qe81kUp7evnrFfUGSC";
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Progress!");
+
+    println!("Devnet url: {}", DEVNET_URL);
 }
